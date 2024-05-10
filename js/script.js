ea52ea809c683ee156726475c73f1dc2fd69f77e
PDeRivas <p.derivas@itecriocuarto.org.ar>
2024-05-09 23:15:26 -0300

    Avances generales

--- js/script.js
+++ js/script.js
@@ -46,89 +46,139 @@ const agregarJugador = async () => {
     }
 };
 
-// Función asíncrona para listar todos los jugadores del equipo
-const listarJugadores = async () => {
+const limpiarContenedor = async (textoTitulo) => {
     let contenedor = document.getElementById("contenedor")
     while (contenedor.firstChild) {
         contenedor.removeChild(contenedor.firstChild)
     }
+    let row1 = document.createElement('div')
+    row1.className = 'row mx-0'
+
+    let col1 = document.createElement('div')
+    col1.className = 'col-4'
+
+    let titulo = document.createElement('h2')
+    titulo.innerHTML = `${textoTitulo}`
+    titulo.className = 'col-4 text-center mx-0 p-3 bg-info border border-info'
+
+    let col2 = document.createElement('div')
+    col2.className = 'col-4'
+
+    row1.appendChild(col1)
+    row1.appendChild(titulo)
+    row1.appendChild(col2)
+
+    contenedor.appendChild(row1)
+    return contenedor
+}
+
+const tarjetaJugador = async (contenedor, jugador, jugadoresJugando, metodo) => {
+    let row = document.createElement('div')
+    row.className = `row justify-content-center mx-0 my-4`
+
+    let card = document.createElement('div')
+    card.className = `card w-50 p-0 text-center bg-success-subtle`
+
+    let nombre = document.createElement('h3')
+    nombre.innerHTML = jugador.nombre
+    card.appendChild(nombre)
+
+    let edad = document.createElement('p')
+    edad.innerHTML = `Edad: ${jugador.edad}`
+    card.appendChild(edad)
+
+    let posicion = document.createElement('p')
+    posicion.innerHTML = `Posicion: ${jugador.posicion}`
+    card.appendChild(posicion)
+
+    let jugando = document.createElement('p')
+    jugando.innerHTML = `${jugador.jugando ? 'Jugando' : 'No esta jugando'} `
+    card.appendChild(jugando)
+
+    let cambiarPosicion = document.createElement('button')
+    cambiarPosicion.innerHTML = 'Cambiar Posicion'
+    cambiarPosicion.type = 'button'
+    cambiarPosicion.className = 'btn btn-info rounded-0'
+    cambiarPosicion.addEventListener('click', (botonApretado) => {
+        nuevaPosicion = prompt("Ingrese la nueva posicion del jugador:");
+        asignarPosicion(jugador.id, nuevaPosicion, metodo)
+    })
+    card.appendChild(cambiarPosicion)
+
+    let botonJugar = document.createElement('button')
+    botonJugar.innerHTML = `${jugador.jugando ? 'Sacar' : 'Poner a Jugar'}`
+    botonJugar.type = 'button'
+    botonJugar.className = `btn btn-${jugador.jugando ? 'danger' : 'success'} rounded-0 rounded-bottom-2`
+    botonJugar.addEventListener('click', (botonApretado) => {
+        ponerAJugar(jugador.id, jugadoresJugando, metodo)
+    })
+    card.appendChild(botonJugar)
+
+    row.appendChild(card)
+    contenedor.appendChild(row)
+}
+
+// Función asíncrona para listar todos los jugadores del equipo
+const listarJugadores = async () => {
+    let contenedor = await limpiarContenedor('Listado de Jugadores')
+
     let jugadores = await obtenerJugadoresLocalStorage()
     let jugadoresJugando = 0
 
     jugadores.forEach(jugador => {
-        if(jugador.jugando){
+        if (jugador.jugando) {
             jugadoresJugando += 1
         }
-        let row = document.createElement('div')
-
-        let card = document.createElement('div')
-
-        let nombre = document.createElement('h3')
-        nombre.innerHTML = jugador.nombre
-        card.appendChild(nombre)
-                
-        let edad = document.createElement('p')
-        edad.innerHTML = `Edad: ${jugador.edad}`
-        card.appendChild(edad)
-
-        let posicion = document.createElement('p')
-        posicion.innerHTML = `Posicion: ${jugador.posicion}`
-        card.appendChild(posicion)
-
-        let jugando = document.createElement('p')
-        jugando.innerHTML = `${jugador.jugando ? 'Jugando': 'No esta jugando'} `
-        card.appendChild(jugando)
-        
-        let cambiarPosicion = document.createElement('button')
-        cambiarPosicion.innerHTML = 'Cambiar Posicion'
-        cambiarPosicion.addEventListener('click', (botonApretado)=>{
-            nuevaPosicion = prompt("Ingrese la nueva posicion del jugador:");
-            asignarPosicion(jugador.id, nuevaPosicion)
-        })
-        card.appendChild(cambiarPosicion)
-        
-        let botonJugar = document.createElement('button')
-        botonJugar.innerHTML = `${jugador.jugando ? 'Sacar':'Poner a Jugar'}`
-        botonJugar.addEventListener('click', (botonApretado) => {
-            ponerAJugar(jugador.id, jugadoresJugando)
-        })
-        card.appendChild(botonJugar)
-
-        row.appendChild(card)
-        contenedor.appendChild(row)
+        tarjetaJugador(contenedor, jugador, jugadoresJugando, listarJugadores)
     }
     )
     console.log(`Cantidad de jugadores jugando ${jugadoresJugando}`)
 };
 
+const verPartido = async () => {
+    let contenedor = await limpiarContenedor('Jugadores en el Partido')
+
+    let jugadores = await obtenerJugadoresLocalStorage()
+    let jugadoresJugando = 0
+
+    jugadores.forEach(jugador => {
+        if (jugador.jugando) {
+            jugadoresJugando += 1
+            tarjetaJugador(contenedor, jugador, jugadoresJugando, verPartido)
+        }
+    }
+    )
+    console.log(`Cantidad de jugadores jugando ${jugadoresJugando}`)
+}
+
 // Función asíncrona para asignar una nueva posición a un jugador
-const asignarPosicion = async (id, nuevaPosicion) => {
+const asignarPosicion = async (id, nuevaPosicion, metodo) => {
     let jugadores = await obtenerJugadoresLocalStorage();
-    try{
+    try {
         posJugador = jugadores.indexOf(jugadores.find(jugador => jugador.id === id))
         jugadores[posJugador].posicion = nuevaPosicion
         guardarJugadoresLocalStorage(jugadores)
-        listarJugadores()
+        metodo()
     }
-    catch(error){
+    catch (error) {
         console.error('Error:', error.message);
     }
 };
 
-const ponerAJugar = async (id, jugadoresJugando) => {
+const ponerAJugar = async (id, jugadoresJugando, metodo) => {
     let jugadores = await obtenerJugadoresLocalStorage();
-    try{
+    try {
         posJugador = jugadores.indexOf(jugadores.find(jugador => jugador.id === id))
-        if((jugadoresJugando == 11 && jugadores[posJugador].jugando) || jugadoresJugando < 11){
+        if ((jugadoresJugando == 11 && jugadores[posJugador].jugando) || jugadoresJugando < 11) {
             jugadores[posJugador].jugando = !jugadores[posJugador].jugando
             guardarJugadoresLocalStorage(jugadores)
-            listarJugadores()
+            metodo()
         }
-        else{
+        else {
             console.log('No se pueden agregar mas jugadores')
         }
     }
-    catch(error){
+    catch (error) {
         console.error('Error:', error.message);
     }
 };
@@ -137,7 +187,7 @@ const ponerAJugar = async (id, jugadoresJugando) => {
 const realizarCambio = async (jugadorEntrante, jugadorSaliente) => {
     // Implementación para realizar un cambio durante un partido
 };
-listarJugadores()
+
 // Función principal asíncrona que interactúa con el usuario
 const main = async () => {
     try {
